851a4e8bd162a2e0ad86c864561f02779e2f51a8
Lynn-Hsiao <lynnlynnlynn935@gmail.com>
2023-07-28 07:09:01 +0800

    Add product data

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,66 +1,78 @@
 import styles from './Cart.module.css'
+import productsData from './productData'
+import { useState } from 'react'
 import { ReactComponent as Plus } from '../../icons/plus.svg'
 import { ReactComponent as Minus } from '../../icons/minus.svg'
 
-const cartItem = [
-  {
-    id: '1',
-    name: '貓咪罐罐',
-    img: 'https://picsum.photos/300/300?text=1',
-    price: 100,
-    quantity: 2,
-  },
-  {
-    id: '2',
-    name: '貓咪干干',
-    img: 'https://picsum.photos/300/300?text=2',
-    price: 200,
-    quantity: 1,
-  },
-]
 
-const totalPrice = cartItem.reduce((accumulator, item) => {
-  const itemTotalPrice = item.price * item.quantity;
-  return accumulator + itemTotalPrice;
-}, 0)
-
-function Item({id, name, img, price, quantity}) {
-  return (
+function Product ({id, name, img, price, quantity, handlePlusClick, handleMinusClick}) {
+  return(
     <div className={styles.productContainer} data-count="0" data-price={price} key={id}>
       <img className={styles.imgContainer} src={img} alt={name}/>
       <div className={styles.productInfo}>
         <div className={styles.productName}>{name}</div>
         <div className={styles.productControlContainer}>
           <div className={styles.productControl}>
-            <Minus className={styles.minus} />
+            <Minus className={styles.minus} onClick={() => handleMinusClick(id, price, quantity)} />
             <span className={styles.productCount}>{quantity}</span>
-            <Plus className={styles.plus} />
+            <Plus className={styles.plus} onClick={() =>handlePlusClick(id, price, quantity)} /> 
+            
           </div>
         </div>
-        <div className={styles.productPrice}>${price}</div>
+        <div className={styles.productPrice}>${price * quantity}</div>
       </div>
-    </div>
-  )
-}
-
-function Product ( ) {
-  return (
-    <>
-    {cartItem.map(item =>
-    //<Item {...item} /> = <Item id={item.id} name={item.name} img={item.img} price={item.price} quantity={item.quantity} /> 
-      <Item {...item} key={item.id} />
-    )}
-    </>
-  )
+    </div>)
 }
 
 function Cart () {
+  const [cartItem, setCartItem ] = useState(productsData)
+  const [totalPrice, setTotalPrice] = useState(0)
+
+  function calculate(price) {
+    console.log(price)
+    setTotalPrice(totalPrice + price)
+  }
+  
+  function handlePlusClick(id, price ,quantity) {
+    const newCartItem = cartItem.map(item => {
+      if (item.id === id) {
+        return {
+          ...item,
+          quantity: quantity + 1,
+        }
+      } else {
+        return item
+      }
+    })
+    setCartItem(newCartItem)
+    calculate(price)
+  }
+
+  function handleMinusClick(id, price ,quantity) {
+    if (quantity > 0) {
+      const newCartItem = cartItem.map(item => {
+      if (item.id === id) {
+        return {
+          ...item,
+          quantity: quantity - 1,
+        }
+      } else {
+        return item
+      }
+      })
+      setCartItem(newCartItem)
+      calculate(-price)
+    }
+  }
+
   return (
     <section className={styles.cartContainer}>
       <h3 className={styles.cartTitle}>購物籃</h3>
 
       <section className={styles.productList} data-total-price={totalPrice}>
-        <Product/>
+        {cartItem.map(item => (
+          <Product key={item.id} {...item} setCartItem={setCartItem} handlePlusClick={handlePlusClick} handleMinusClick={handleMinusClick}/>
+        ))}
       </section>
 
       <section className={styles.cartInfo}>
@@ -78,32 +90,10 @@ function Cart () {
 
 export default Cart;
 
-// 原始CartItem資料
-// <div className={styles.productContainer} data-count="0" data-price="3999">
-//   <img className={styles.imgContainer} src="/images/product-1.jpg" alt="product-1"/>
-//   <div className={styles.productInfo}>
-//     <div className={styles.productName}>破壞補丁修身牛仔褲</div>
-//     <div className={styles.productControlContainer}>
-//       <div className={styles.productControl}>
-//         <Minus className={styles.minus} />
-//         <span className={styles.productCount}>1</span>
-//         <Plus className={styles.plus} />
-//       </div>
-//     </div>
-//     <div className={styles.productPrice}>$3,999</div>
-//   </div>
-// </div>
-// <div className={styles.productContainer} data-count="0" data-price="1299">
-//   <img className={styles.imgContainer} src="/images/product-2.jpg" alt="product-2" />
-//   <div className={styles.productInfo}>
-//     <div className={styles.productName}>刷色直筒牛仔褲</div>
-//     <div className={styles.productControlContainer}>
-//       <div className={styles.productControl}>
-//         <Minus className={styles.minus} />
-//         <span className={styles.productCount}>1</span>
-//         <Plus className={styles.plus} />
-//       </div>
-//     </div>
-//     <div className={styles.productPrice}>$1,299</div>
-//   </div>
-// </div>
+
+
+  // 計算totalPrice
+  // const totalPrice = cartItem.reduce((accumulator, item) => {
+  // const itemTotalPrice = item.price * item.quantity;
+  // return accumulator + itemTotalPrice;
+  // }, 0)
