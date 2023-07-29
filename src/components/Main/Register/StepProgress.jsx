aa95c7a1bbe3e4038241bdbe6d925b5a9babc9a3
Lynn-Hsiao <lynnlynnlynn935@gmail.com>
2023-07-29 23:19:46 +0800

    Adjust color and structure to fit each progress

--- src/components/Main/Register/StepProgress.jsx
+++ src/components/Main/Register/StepProgress.jsx
@@ -10,19 +10,21 @@ function StepProgress () {
       <section className={styles.progressContainer}>
           <span className={styles.progressGroup} data-phase="address">
             <span className={styles.progressIcon}>
-              {/* <span className={styles.text}>1</span> */}
-              <PgComplete className={styles.completeIcon}/>
+              <span className={styles.text}>1</span>
+              {/* <PgComplete className={styles.completeIcon}/> */}
             </span>
             <span className={styles.progressLabel}>寄送地址</span>
+            <span className={styles.progressBar} data-order="1"></span>
           </span>
-          <span className={styles.progressBar} data-order="1"></span>
+          
           <span className={styles.progressGroup} data-phase="shipping">
             <span className={styles.progressIcon}>
               <span className={styles.text}>2</span>
             </span>
             <span className={styles.progressLabel}>運送方式</span>
+            <span className={styles.progressBar} data-order="2"></span>
           </span>
-          <span className={styles.progressBar} data-order="2"></span>
+          
           <span className={styles.progressGroup} data-phase="credit-card">
             <span className={styles.progressIcon}>
               <span className={styles.text}>3</span>
