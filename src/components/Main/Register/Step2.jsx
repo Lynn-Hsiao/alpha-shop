f3104be9167c079757b041ad1704ea9778799209
Lynn-Hsiao <lynnlynnlynn935@gmail.com>
2023-08-14 01:28:29 +0800

    Remove " Checked " status

--- src/components/Main/Register/Step2.jsx
+++ src/components/Main/Register/Step2.jsx
@@ -1,23 +1,15 @@
 import styles from './Step2.module.css'
-import { useState } from 'react'
+// import { useState } from 'react'
 
 
 function Step2 () {
-  const [isChecked1, setIsChecked1] = useState(false)
-  const handleClick1 = () => {
-  setIsChecked1(!isChecked1)
-}
 
-  const [isChecked2, setIsChecked2] = useState(false)
-  const handleClick2 = () => {
-  setIsChecked2(!isChecked2)
-}
   return (
     <form data-phase="shipping">
       <h3 className={styles.formTitle}>運送方式</h3>
       <section className={styles.fromBodyItem}>
         <label className={styles.radioGroup} data-price="0">
-          <input className={styles.inputStyle} id="shipping-standard" type="radio" name="shipping" onClick={handleClick1} checked={isChecked1} />
+          <input className={styles.inputStyle} id="shipping-standard" type="radio" name="shipping" />
           <div className={styles.radioInfo}>
             <div>
               <div className={styles.text}>標準運送</div>
@@ -28,7 +20,7 @@ function Step2 () {
           <div className={styles.radioBoxBorder}>免費</div>
         </label>
         <label className={styles.radioGroup} data-price="500">
-          <input className={styles.inputStyle} id="shipping-dhl" type="radio" name="shipping" onClick={handleClick2} checked={isChecked2} />
+          <input className={styles.inputStyle} id="shipping-dhl" type="radio" name="shipping" />
           <div className={styles.radioInfo}>
             <div>
               <div className={styles.text}>DHL 貨運</div>
